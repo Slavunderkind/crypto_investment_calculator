15d6349f04faf5a95bd25f5791c9ab6532cb5dfd
Slavka Tsankova <smcankova@gmail.com>
2017-10-07 19:39:11 +0300

    List all cryptos from coinmarketcap API

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -3,11 +3,20 @@ require 'net/http'
 class CurrenciesController < ApplicationController
   def new
     uri = URI(COINMARKETCAP_ENDPOINT)
-    @all = Net::HTTP.get_response(uri).body # => String
+    res = Net::HTTP.get_response(uri)
+    @all = JSON.parse(res.body)
+    @crypto_names = all_crypto_names
   end
 
-  def index
-  end
+  def index; end
 
   def create; end
+
+  private
+
+  def all_crypto_names
+    @all.each_with_object([]) do |line, result|
+      result << line['id']
+    end
+  end
 end
