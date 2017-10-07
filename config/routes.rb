5e17f4dec9fd1ec2663b19bb0b5634be51ff4a70
Slavka Tsankova <smcankova@gmail.com>
2017-10-07 16:24:04 +0300

    Rename controller and view to Currencies and refactor input form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'currency/new'
-
-  get 'pages/index'
-  root "pages#index"
+  root 'pages#index'
+  resources :currencies, only: %i[new create index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
